8fc73f7215a7f0cc5bc3a7ff547d1e8c30c7f9af
Home User <home@example.com>
2026-07-17 00:18:15 -0300

    chore: populate exercise files with content

--- challenges/aula03-create-db.sql
+++ challenges/aula03-create-db.sql
@@ -4,15 +4,4 @@
 -- Criado banco de dados "empresa"
 CREATE DATABASE empresa; 
 
--- Criar tabela
-CREATE TABLE usuarios (
-    id int auto_increment primary key,
-    nome VARCHAR (100),
-    email VARCHAR (100),
-    idade int,
-);
--- Arrumado o nome dos dados das tabelas, seus tipos e suas especficações 
-
-
--- Seleciona todos os dados de usuarios e mostra eles
-SELECT * FROM usuarios
+USE empresa;
